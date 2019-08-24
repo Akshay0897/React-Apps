fab171bbddcfc01e00cb571d95017e46b971dac8
Akshay <akshayaghera05@gmail.com>
2019-08-24 18:09:10 +0530

    Sample react project - ecommerce

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className = 'homepage'>
+        <div className = 'directory-menu'>
+            <div className = 'menu-item'>
+                <div className='content'>
+                    <h1 className='title'>
+                        Mens
+                    </h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className='content'>
+                    <h1 className='title'>
+                        Women
+                    </h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className='content'>
+                    <h1 className='title'>
+                        Childeren
+                    </h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+)
+
+export default Homepage;
