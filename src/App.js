64fc5b0f4cd873a48b37482a46a2a0da5499871b
= <=>
2019-08-25 15:23:50 +0530

    sample router implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch,Route } from 'react-router-dom';
 import Homepage from './components/pages/homepage/homepage.component';
+
+
+const HatsPage = (props) => ( 
+  <div>
+    <h1>
+      Hats Page
+    </h1>
+  </div>
+);
+ 
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/hats' render={routeProps => <HatsPage {...routeProps}/> } />
+      </Switch>
     </div>
   );
 }
